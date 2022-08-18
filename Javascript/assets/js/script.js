f21bb0f6a6fffeda91cdba82e3b3bba8c10ab75b
mahammadalirk <mahammadalirk@code.edu.az>
2022-08-18 07:16:41 +0400

    JavaScript

---
+++ Javascript/assets/js/script.js
@@ -0,0 +1,137 @@
+// var,let,const
+
+
+
+// console.log("Hello");
+
+
+
+
+// var name;
+// let age;
+// var name = "Huseyn";
+// var surname = "Ceferov";
+// console.log(name + " " + surname);
+// console.log(`${name} ${surname}`);
+// let surname1 = "Jafarov";
+// console.log(surname1);
+
+// const codeClass = "Programming";
+// console.log(codeClass);
+// const age = 33;
+// let age1 = 33;
+// let isMarried = true;
+// let num1 = 55;
+// let adress = null;
+// console.log(adress);
+// let person = {
+//     name: "Cavid",
+//     surname: "Bashirov",
+//     age: 33,
+//     phoneNumbers: [3455, 67565]
+// }
+// let person1 = {
+//     name: "Orxan",
+//     surname: "Akberov",
+//     age: 25,
+//     phoneNumbers: [13452, 99898]
+// }
+
+// let students = [{
+//     name: "Nermin",
+//     surname: "Memmedova",
+//     age: 30,
+//     phoneNumbers: [56767, 3897]
+// },
+// {
+//     name: "Huseyn",
+//     surname: "Jafarov",
+//     age: 23,
+//     phoneNumbers: [111111, 222222]
+// },
+// {
+//     name: "Maga",
+//     surname: "Kerimov",
+//     age: 28,
+//     phoneNumbers: [456546, 666666]
+// }];
+
+// for (let i = 0; i < students.length; i++) {
+
+//     if (students[i].age <= 23) {
+//         console.log(students[i].age + " " + students[i].name);
+
+//     } else {
+//         console.log("Non student");
+//     };
+
+// }
+
+// for (let i = 0; i < student.length; i++) {
+//     for (let j = 0; j < student[i].phoneNumbers.length; j++) {
+//         console.log(student[i].phoneNumbers[j]);
+
+//     };
+
+// };
+// for (let i = 0; i < person.phoneNumbers.length; i++) {
+//     console.log(person.phoneNumbers[i]);
+
+// };
+// console.log(person1, person);
+
+// let student = ["Orxan", "Cavidan", 26, 20];
+// for (let i = 0; i < student.length; i++) {
+
+//     console.log(student[0]);
+// }
+
+// let numbers = [1, 2, 3, 4, 5, 6, 8, 8, 8, 9, 9, 8, 10]
+// numbers.forEach(item => {
+//     console.log(item);
+// });
+
+// let num1 = 64;
+// let num2 = 32;
+
+// let result = num1 % num2;
+// console.log(result);
+
+// for (let i = 0; i <= 20; i++) {
+//     if (i == 0) {
+//         console.log("Ne tek ne de cutdur");
+
+//     } else {
+//         if (i % 2 == 1) {
+//             console.log("Tek :" + i);
+//         } else {
+//             console.log("Cut :" + i);
+//         }
+//     }
+
+// }
+// let isMarried = true;
+// if (!isMarried) {
+
+//     console.log("Evli");
+// } else {
+//     console.log("Subay");
+// }
+
+// let result = isMarried ? "Evli" : "Subay";
+// console.log(result);
+
+// let surname = null
+// let result = surname ?? "Bilmirem";
+// console.log(result);
+// let a = 15;
+// let b = a;
+// b = 8;
+// console.log(a);
+// console.log(b);
+// let obj1 = {
+//     name: "Orxan",
+//     surname: "Akberov",
+//     age: 40,
+//     phoneNumbers: [514548451, 4851451],
+// };
